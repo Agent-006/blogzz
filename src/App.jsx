65f28a6bdca08a48408f47e69e809dbe9641c3dc
Sagar~ <sagargope2001@gmail.com>
2024-01-25 01:49:57 +0530

    chore: setup of appwrite backend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+
+  console.log(conf.appwriteUrl)
+
+  return (
+    <>
+      <h1>Blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
