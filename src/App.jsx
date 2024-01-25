da34df445d8bd1a462bbd89b46a2c71063638224
Sagar~ <sagargope2001@gmail.com>
2024-01-26 03:17:18 +0530

    chore: redux toolkit setup, all components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import './App.css'
-import conf from './conf/conf'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(conf.appwriteUrl)
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
-    <>
-      <h1>Blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap justify-center items-center bg-zinc-500">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select/Select";
+import Signup from "./Signup/Signup";
+import Login from "./Login/Login";
+import Input from "./Input/Input";
+import Button from "./Button/Button";
+import AuthLayout from "./AuthLayout/AuthLayout";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Select,
+    Signup,
+    Login,
+    Input,
+    Button,
+    AuthLayout,
+}
